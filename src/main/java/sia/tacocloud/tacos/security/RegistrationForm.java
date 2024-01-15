3fe12648c7dc0d5b3afc5924fb9f9aca2df9f2e8
pavel <pavel.polyakov0010@gmail.com>
2024-01-15 20:20:08 +0100

    Chapter 5 - Security Login page and controller. Security settings.

--- src/main/java/sia/tacocloud/tacos/security/RegistrationForm.java
+++ src/main/java/sia/tacocloud/tacos/security/RegistrationForm.java
@@ -14,6 +14,70 @@ public class RegistrationForm {
     private String phoneNumber;
 
     public User toUser(PasswordEncoder passwordEncoder) {
-        return new User(username, password, fullName, state, city, state, zip, phoneNumber);
+        return new User(username, passwordEncoder.encode(password), fullName, state, city, state, zip, phoneNumber);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 }
--- src/main/java/sia/tacocloud/tacos/security/SecurityConfig.java
+++ src/main/java/sia/tacocloud/tacos/security/SecurityConfig.java
@@ -2,14 +2,38 @@ package sia.tacocloud.tacos.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig{
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/design", "/orders").hasRole("USER")
+                        .requestMatchers("/", "/**").permitAll()
+                        .anyRequest().authenticated()
+                        .and()
+                        .formLogin()
+                        .loginPage("/login")
+                        .and()
+                        .oauth2Login()
+                        .loginPage("/login")
+                        .and()
+                        .logout()
+                        .logoutSuccessUrl("/")
+                        .defaultSuccessUrl("/design"))
+                .build();
     }
 }
